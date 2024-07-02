625fd803ce64a9ddee13466e780270aec7c632a0
being-iota <ratnapriya2954@gmail.com>
2024-07-02 22:37:17 +0530

    update 2nd July

---
+++ Practice/1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    for(int i=0;i<10;i++){
+        cin>>arr[i];
+        arr[i]=2*arr[i];
+    }
+    return 0;
+}
