eda93d270635aec7fe1890258a853971604b3cf7
being-iota <ratnapriya2954@gmail.com>
2024-07-03 23:01:33 +0530

    Updated 03 July 2024

---
+++ July/Array_codes/Easyqs practice/TwoSum.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) { 
+        unordered_map<int, int> numMap;
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                // Ensure smaller index comes first
+                result.push_back(numMap[complement]);
+                result.push_back(i);
+                break;
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return result;
+    }
+};
